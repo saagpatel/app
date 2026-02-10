5bbe04f6e2874e6921faea1d4b2e8b174d642d79
Saagar <saagar210@gmail.com>
2026-02-09 21:59:06 -0800

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by D on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
